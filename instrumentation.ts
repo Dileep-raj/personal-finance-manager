c999c8c8309496895e86f3d769bce80ee59ef655
dileep <dileeprajb949@gmail.com>
2026-08-05 21:01:03 +0530

    Added instrumentation to verify MongoDB connection before server startup

---
+++ instrumentation.ts
@@ -0,0 +1,11 @@
+import { connectToDatabase } from "./lib/mongoose"
+
+
+export async function register() {
+    console.log("Server starting up...")
+    const connection = await connectToDatabase()
+    if (connection) console.log("Connected to MongoDB successfully")
+    else {
+        throw new Error("MongoDB connection failed!\nExiting server")
+    }
+}
